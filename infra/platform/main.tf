d067617577395831013aae2da189a34cb4f8dbe9
yuto.eguma <yuto.eguma@gmail.com>
2021-10-01 17:54:33 +0900

    VMインスタンスの作成

--- infra/platform/main.tf
+++ infra/platform/main.tf
@@ -4,6 +4,7 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
+  zone    = "asia-northeast1-a"
 }
 
 module "naming" {
@@ -14,4 +15,35 @@ module "naming" {
 resource "google_service_account" "account" {
   account_id   = module.naming.name
   display_name = module.naming.name
+}
+
+resource "google_compute_instance" "default" {
+  name         = module.naming.name
+  machine_type = "e2-medium"
+  zone         = "asia-northeast1-a"
+
+  tags = [module.naming.name]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  } 
+
+  # scratch_disk {
+  #   interface = "SCSI"
+  # }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemral public IP
+    }
+  }
+
+  service_account {
+    email = google_service_account.account.email
+    scopes = [ "cloud-platform" ]
+  }
 }
