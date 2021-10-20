419bc06d6cf9a575d6565e81a809fe91c25f7a99
yuto.eguma <yuto.eguma@gmail.com>
2021-10-21 00:38:29 +0900

    ファイルに追記するだけで実行されるように

--- infra/tf_create_all.sh
+++ infra/tf_create_all.sh
@@ -6,5 +6,7 @@ TF_CMD=$1
 ENV=$2
 TF_ARGS=${@:3}
 
-./tf.sh $TF_CMD $ENV platform/vpn $TF_ARGS
-./tf.sh $TF_CMD $ENV platform/vm $TF_ARGS
+cat directries.txt | while read line
+do 
+    echo "./tf.sh $TF_CMD $ENV $line $TF_ARGS"
+done
